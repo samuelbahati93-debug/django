6f61e16b79ae00efaeed319efb62a2f985fe0e6a
samuelbahati93-debug <samuelbahati93@gmail.com>
2026-03-24 12:08:56 +0300

    myproject

--- myproject/main/views.py
+++ myproject/main/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import todolist,item
 
+
+def index(request,name):
+    ls = todolist.objects.get(name=name)
+    items = ls.item_set.get(id=1)
+    return HttpResponse("<h1>%s</h1><p>%s</p>"%(ls.name, items.text))
 # Create your views here.
