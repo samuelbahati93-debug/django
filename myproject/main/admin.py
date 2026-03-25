75146fba1fb40fb08dd0f696b7bc8bbe8541ba93
samuelbahati93-debug <samuelbahati93@gmail.com>
2026-03-25 17:25:24 +0300

    myproject

--- myproject/main/admin.py
+++ myproject/main/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from .models import todolist,item
+admin.site.register(todolist)
+admin.site.register(item)
 # Register your models here.
--- myproject/main/views.py
+++ myproject/main/views.py
@@ -3,8 +3,9 @@ from django.http import HttpResponse
 from .models import todolist,item
 
 
-def index(request,name):
-    ls = todolist.objects.get(name=name)
-    items = ls.item_set.get(id=1)
-    return HttpResponse("<h1>%s</h1><p>%s</p>"%(ls.name, items.text))
+def index(response,id):
+    ls = todolist.objects.get(id=id)
+    return render(response,"main/list.html",{"ls":ls})
 # Create your views here.
+def home(response):
+    return render(response,"main/home.html",{})
